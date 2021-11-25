30f870a7cbaef3633b3cfbbae28b4f5179dee66a
Clément David <david.clement5106@gmail.com>
2021-11-25 14:16:41 +0100

    Finitions du loader

---
+++ js/global.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const loader = document.getElementById('loader');
+        loader.style.animation = 'slide-up .5s forwards';
+    }, 1000);
+});
+
